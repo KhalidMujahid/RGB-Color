989553f3daa948d49d74226ab4be8dde5e9209c0
khalidMujahid <binkhalid267@gmail.com>
2021-05-30 18:41:35 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const colorChanger = () => {
+  const red = document.querySelector("#red").value;
+  const blue = document.querySelector("#blue").value;
+  const green = document.querySelector("#green").value;
+  const hTag = document.querySelector("h1").value;
+  
+  const styles = 'rgb('+ red  + ' , ' + green + ' , ' + blue + ')';
+  
+  document.body.style.backgroundColor = styles;
+  hTag.style.backgroundColor = styles;
+  document.querySelector("#result").value = styles;
+}
+
+document.querySelector("#red").addEventListener("input", colorChanger);
+document.querySelector("#blue").addEventListener("input", colorChanger);
+document.querySelector("#green").addEventListener("input", colorChanger);
